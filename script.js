8c027f69c51cbf1274b079248d68492298140521
Yash Chaddha <yash.chaddha1997@gmail.com>
2023-07-04 00:44:30 +0530

    Working Backend

---
+++ script.js
@@ -0,0 +1,33 @@
+var dropdown=$('#dog-breeds');
+var dogImage=$('#breed-image');
+var breed;
+
+//Get all the breeds to populate the dropdown
+$.get('https://dog.ceo/api/breeds/list/all',function(data){
+    let dogBreeds=data.message;
+    for (let breed in dogBreeds){
+        console.log(breed);
+        dropdown.append('<option value="'+breed+'">'+ breed + '</option>')
+    }
+});
+
+$('#get-image').click(function(e){
+    breed=dropdown.val();
+    displayImage(breed);
+});
+
+$('#next').click(function(event){
+    displayImage(breed);
+})
+
+
+function displayImage(x){
+    let url='https://dog.ceo/api/breed/'+breed+'/images/random';
+    $.get(
+        url,
+        function(d){
+            console.log(d.message);
+            dogImage.attr('src',d.message);
+        }
+    );
+}
